539132da08ed85e5734e1182dbf7cca6abe4f485
dotjarvis <johnsonmakings@gmail.com>
2024-04-01 18:56:58 +0300

    Added TaiwindCSS

--- react_jobs/src/App.jsx
+++ react_jobs/src/App.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 const App = () => {
   return (
-    <div>
-      <h1>Name</h1>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
   );
 };
 
