5b8e2886fe5f0c3377b892a277f75b30c944740d
dotjarvis <johnsonmakings@gmail.com>
2024-04-02 16:57:16 +0300

    Json file & job listing

--- react_jobs/src/components/HomeCards.jsx
+++ react_jobs/src/components/HomeCards.jsx
@@ -7,7 +7,6 @@ const HomeCards = () => {
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          {/* <div className="bg-gray-100 p-6 rounded-lg shadow-md"> */}
           <Card bg="bg-gray-100">
             <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
@@ -20,9 +19,7 @@ const HomeCards = () => {
               Browse Jobs
             </a>
           </Card>
-          {/* </div> */}
 
-          {/* <div className="bg-indigo-100 p-6 rounded-lg shadow-md"> */}
           <Card bg="bg-indigo-100">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
@@ -35,7 +32,6 @@ const HomeCards = () => {
               Add Job
             </a>
           </Card>
-          {/* </div> */}
         </div>
       </div>
     </section>
