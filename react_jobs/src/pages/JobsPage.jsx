626fc5210c7d6e2fc0f4ca48851359e5eaf5a94b
dotjarvis <johnsonmakings@gmail.com>
2024-04-03 14:54:51 +0300

    JobsPage & props

--- react_jobs/src/pages/JobsPage.jsx
+++ react_jobs/src/pages/JobsPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import JobListings from "../components/JobListings";
 
 const JobsPage = () => {
-  return <div>Jobs</div>;
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <JobListings isHome={false} />
+    </section>
+  );
 };
 
 export default JobsPage;
