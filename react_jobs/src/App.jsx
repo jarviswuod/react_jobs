42bf5aab9c5627b1b00798c5d92f92347953ab9b
dotjarvis <johnsonmakings@gmail.com>
2024-04-03 14:28:51 +0300

    Links & custom 404

--- react_jobs/src/App.jsx
+++ react_jobs/src/App.jsx
@@ -9,15 +9,15 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import AddJobPage from "./pages/AddJobPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
-  // createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
-  // createRoutesFromElements(<Route index element={<HomePage />} />)
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
       <Route path="/add-job" element={<AddJobPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
