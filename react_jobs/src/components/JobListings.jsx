c3eca9a4f61b083a25b3ee641dfd8be9db78c943
dotjarvis <johnsonmakings@gmail.com>
2024-04-04 06:17:43 +0300

    Loaing spinner in reactjs

--- react_jobs/src/components/JobListings.jsx
+++ react_jobs/src/components/JobListings.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 
-// import jobs from "../jobs.json";
 import JobListing from "./JobListing";
+import Spinner from "./ClipLoader";
 
 const JobListings = ({ isHome = false }) => {
-  // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
-
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome
+        ? "http://localhost:8000/jobs?_limit=3"
+        : "http://localhost:8000/jobs";
       try {
-        const res = await fetch("http://localhost:8000/jobs");
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
@@ -30,44 +31,18 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {loading ? (
-            <h2>Loading</h2>
-          ) : (
-            <>
-              {/* {jobListings.map((job) => ( */}
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-            </>
-          )}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
 };
 
 export default JobListings;
-
-// import jobs from "../jobs.json";
-// import JobListing from "./JobListing";
-
-// const JobListings = ({ isHome }) => {
-//   const jobListings = isHome ? jobs.slice(0, 3) : jobs;
-//   return (
-//     <section className="bg-blue-50 px-4 py-10">
-//       <div className="container-xl lg:container m-auto">
-//         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-//           {isHome ? "Recent Jobs" : "Browse jobs"}
-//         </h2>
-//         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-//           {jobListings.map((job) => (
-//             <JobListing key={job.id} job={job} />
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default JobListings;
