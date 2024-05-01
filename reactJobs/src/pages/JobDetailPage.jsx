4e63dc2a89eafa78fc4704a9c413fb94d73a6b54
dotjarvis <johnsonmakings@gmail.com>
2024-05-01 21:34:04 +0300

    Fixing bugs from useLoaderData

--- reactJobs/src/pages/JobDetailPage.jsx
+++ reactJobs/src/pages/JobDetailPage.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { Link, useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const JobDetailPage = () => {
-  const { id } = useParams();
   const job = useLoaderData();
 
   return (
@@ -16,50 +13,10 @@ const JobDetailPage = () => {
   );
 };
 
-const jobLoader = async ({ param }) => {
-  const res = await fetch(`/api/jobs/${param.id}`);
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
   const data = await res.json();
   return data;
 };
 
 export { JobDetailPage as default, jobLoader };
-
-// import React, { useEffect } from "react";
-// import { useState } from "react";
-// import { useParams } from "react-router-dom";
-
-// const JobDetailPage = () => {
-//   const { id } = useParams();
-//   const [loading, setLoading] = useState(true);
-//   const [jobDetail, setJobDetail] = useState(true);
-
-//   useEffect(() => {
-//     const jobdetail = async () => {
-//       try {
-//         const res = await fetch(`/api/jobs/${id}`);
-//         const data = await res.json();
-//         console.log(res);
-//         console.log(data);
-//         setJobDetail(data);
-//       } catch (error) {
-//         console.log(`An error occurred ${error}`);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     jobdetail();
-//   }, []);
-
-//   return loading ? (
-//     <p>loading ...</p>
-//   ) : (
-//     <>
-//       <p>{jobDetail.title}</p>
-//       <p>{jobDetail.type}</p>
-//       <p>{jobDetail.description}</p>
-//     </>
-//   );
-// };
-
-// export default JobDetailPage;
